38f79dc1fcc495d4c0ae712034a44120c0b6a601
frankNowinski <nowinski.frank@gmail.com>
2016-06-26 22:28:46 -0400

    first commit

---
+++ config/environment.rb
@@ -0,0 +1,4 @@
+require 'bundler/setup'
+require 'pry-byebug'
+
+Bundler.require(:default)
---
+++ lib/apple_stocks.rb
@@ -0,0 +1,18 @@
+require_relative '../config/environment'
+
+def get_max_profit(stock_prices_yesterday)
+  raise IndexError, 'Must provide more than 2 stock prices' if stock_prices_yesterday.length < 2
+
+  min_price = stock_prices_yesterday.first
+  max_profit = stock_prices_yesterday[1] - min_price
+
+  stock_prices_yesterday[1..-1].each do |current_stock|
+    potential_profit = current_stock - min_price
+    max_profit = potential_profit if potential_profit > max_profit
+    min_price = [min_price, current_stock].min
+  end
+  max_profit
+end
+
+puts get_max_profit([10, 7, 5, 8, 11, 9])
+# puts get_max_profit([10, 7, 5])
